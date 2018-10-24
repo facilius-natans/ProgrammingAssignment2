1ead361cfc1e37201da02867e87c7efe99232d51
facilius-natans <facilius.natans@gmail.com>
2018-10-24 17:41:15 -0400

    These are my cachematrix functions

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,60 @@
-## Put comments here that give an overall description of what your
-## functions do
 
-## Write a short comment describing this function
+## makeCacheMatrix function creates a special "matrix" object that can cache its inverse
 
+# define matrix for future testing
+my_matrix <- matrix(c(1, 5, 9, 10), nrow = 2, ncol = 2)
+
+# create function with "matrix" as argument
+# set initial "inverse" value to NULL
 makeCacheMatrix <- function(x = matrix()) {
-
+  inv <- NULL
+  
+  # create function to cache in the "inverse" value
+  set <- function(y) {
+    x <<- y
+    inv <<- NULL
+  }
+  
+  # function to get the "inverse" value
+  get <- function() x
+  
+  # calculate the "inverse" value
+  setInverse <- function(solve) inv <<- solve
+  
+  # get the "inverse" value
+  getInverse <- function() inv
+  
+  # pass the "inverse" value to the list
+  list(set = set, get = get,
+       setInverse = setInverse,
+       getInverse = getInverse)
 }
 
+#create new variable to be used by "cacheSolve" function
+mm <- makeCacheMatrix(my_matrix)
 
-## Write a short comment describing this function
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+
+## cacheSolve function computes the inverse of the special "matrix" returned by 
+## makeCacheMatrix above. If the inverse has already been calculated (and the matrix 
+## has not changed), then the cacheSolve should retrieve the inverse from the cache.
+  #Create function to get cache of the matrix
+  cacheSolve <- function(x, ...) {
+  inv <- x$getInverse()
+  
+  # get "inverse" data if it is cached
+  if(!is.null(inv)) {
+    message("getting cached data")
+    return(inv)
+  }
+  
+  # calculate the "inverse" value, if it is not cached
+  data <- x$get()
+  inv <- solve(data, ...)
+  x$setInverse(inv)
+  inv
 }
+
+# Use previous function variable "mm"as argument for "cacheSolve" function
+mm <- makeCacheMatrix(my_matrix)
+print(cacheSolve(mm))
