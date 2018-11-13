7812cf1752742f3024aa304de5fe25f512ae6271
facilius-natans <facilius.natans@gmail.com>
2018-11-12 21:40:49 -0500

    Add working logic to the rankhospital function.

--- 3/rankhospital.R
+++ 3/rankhospital.R
@@ -1,7 +1,6 @@
 
-best <- function(state, outcome_name, num) {
-  
-   column_name <-switch(outcome_name,
+rankhospital <- function(state, outcome, num){
+   column_name <-switch(outcome,
          "heart attack" = "Hospital.30.Day.Death..Mortality..Rates.from.Heart.Attack",
          "heart failure" = "Hospital.30.Day.Death..Mortality..Rates.from.Heart.Failure",
          "pneumonia" = "Hospital.30.Day.Death..Mortality..Rates.from.Pneumonia",
@@ -14,27 +13,37 @@ best <- function(state, outcome_name, num) {
     stop("Invalid State")
   }
   
-  outcome <- read.csv("outcome-of-care-measures.csv",  colClasses = "character")
+  input_df <- read.csv("outcome-of-care-measures.csv",  colClasses = "character")
   
-  # Select three required columns: Hospital.Name, State, and column name selected based on the outcome_name param
-  new_outcome <- outcome[, c("Hospital.Name", "State", column_name)]
-  
-  # Select specified State
-  new_outcome <- subset(new_outcome, new_outcome[,"State"] == state)
+  # Select the required columns: Hospital.Name, State, and the column name selected based on the outcome_name param
+  input_df <- input_df[, c("Hospital.Name", "State", column_name)]
 
+  # Select specified State
+  input_df <- subset(input_df, input_df[,"State"] == state)
+  # Format NAs
+  input_df[, column_name] <- gsub("Not Available", "NA", input_df[, column_name])
+  
   if (num == "best") {
-    result <- apply(new_outcome, 2, min)
+    result <- min(as.numeric(input_df[, column_name]), na.rm = TRUE)
   } else if (num == "worst") {
-    result <- apply(new_outcome, 2, max)
+    result <- max(as.numeric(input_df[, column_name]), na.rm = TRUE)
   } else {
-    ranked_outcome <- new_outcome[order(new_outcome[2,]),]
-    result <- ranked_outcome[num, "Hospital.Name"]
+    # Sort by the value specified in the outcome param
+    outcome_sort <- sort(as.numeric(input_df[, column_name]))
+    # Make sure the row specified in the num param exists
+    if (length(outcome_sort) < num) {
+      return("NA")
+    }
+    # Select the outcome value of the specified rank
+    result <- outcome_sort[num]
   }
+
+  # Select the row corresponding to the found value
+  result_row <- subset(input_df, input_df[, column_name] == result)
+  # Remove column label
+  result_hospital <- unname(result_row['Hospital.Name'])
   
   
-  # Get lowest rate for specified outcome
-  #col_name_min <- apply(new_outcome, 2, min)
-  
-  # Remove column name from outcome, and return it
-  unname(result['Hospital.Name'])
+  print(result_hospital)
+  result_hospital
 }
