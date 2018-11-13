0b543ddab090a15570f79e8bacc35c563a3b355d
facilius-natans <facilius.natans@gmail.com>
2018-11-12 23:01:50 -0500

    Improve the Best functioImprove the Best function.

--- 3/best.R
+++ 3/best.R
@@ -4,19 +4,31 @@ best <- function(state, outcome_name){
   column_name <- switch(outcome_name,
          "heart attack" = "Hospital.30.Day.Death..Mortality..Rates.from.Heart.Attack",
          "heart failure" = "Hospital.30.Day.Death..Mortality..Rates.from.Heart.Failure",
-         "pneumonia" = "Hospital.30.Day.Death..Mortality..Rates.from.Pneumonia")
+         "pneumonia" = "Hospital.30.Day.Death..Mortality..Rates.from.Pneumonia",
+         "NA")
   
-  outcome <- read.csv("outcome-of-care-measures.csv",  colClasses = "character")
+  # handle errors
+  if (column_name == "NA") {
+    return(column_name)
+  }
+  if (match(state, state.abb, nomatch = FALSE) == 0) {
+    stop("Invalid State")
+  }
+  
+  df <- read.csv("outcome-of-care-measures.csv",  colClasses = "character")
   
   # Select three required columns: Hospital.Name, State, and column name selected based on the outcome_name param
-  new_outcome <- outcome[, c("Hospital.Name", "State", column_name)]
+  df <- df[, c("Hospital.Name", "State", column_name)]
   
   # Select specified State
-  new_outcome <- subset(new_outcome, new_outcome[,"State"] == state)
+  df <- subset(df, df[,"State"] == state)
 
   # Get lowest rate for specified outcome
-  col_name_min <- apply(new_outcome, 2, min)
-  
-  # Remove column name from outcome, and return it
-  unname(col_name_min['Hospital.Name'])
+  df[, column_name] <- gsub("Not Available", "NA", df[, column_name])
+  col_name_min <- min(as.numeric(df[, column_name]), na.rm = TRUE)
+
+  # Select the row corresponding to the found value
+  result_row <- subset(df, df[, column_name] == col_name_min)
+  # Remove column label
+  result_hospital <- unname(result_row[1, 'Hospital.Name'])
 }
