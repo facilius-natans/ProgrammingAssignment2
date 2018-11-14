23cd24632fdf0ead1571e5fa71315101c719c92a
facilius-natans <facilius.natans@gmail.com>
2018-11-14 19:55:37 +0000

    My draft of function rankall

---
+++ 3/rankall.R
@@ -0,0 +1,54 @@
+#rankall <- function(outcome){
+            
+            #read file
+            df <- read.csv("outcome-of-care-measures.csv", colClasses = "character")
+
+            setnames(df,
+                       old = c("Hospital.30.Day.Death..Mortality..Rates.from.Heart.Attack",
+                       "Hospital.30.Day.Death..Mortality..Rates.from.Heart.Failure",
+                       "Hospital.30.Day.Death..Mortality..Rates.from.Pneumonia"),
+                        new = c("heart attack" , "heart failure" , "pneumonia")
+                       )
+
+            #column_name <- c("heart attack", "heart failure", "pneumonia")
+             df$"heart attack" <- as.numeric(as.character(df$"heart attack"))
+             df$"heart failure" <- as.numeric(as.character(df$"heart failure"))
+             df$"pneumonia" <- as.numeric(as.character(df$"pneumonia"))
+             
+             
+             # Select the required columns: Hospital.Name, State, and the column name with desired outcome
+  
+            df <- df[, c("Hospital.Name", "State", "heart failure")]
+            #print(head(df))
+            
+            df_group_by_state <- group_by(df, "State")
+            print(df_group_by_state)
+            
+            h_a_sorted_col <- sort(df_group_by_state[, "heart failure"])
+            print(h_a_sorted_col)
+  
+  
+  # if (num == "best") {
+  #   result <- min(as.numeric(input_df[, column_name]), na.rm = TRUE)
+  # } else if (num == "worst") {
+  #   result <- max(as.numeric(input_df[, column_name]), na.rm = TRUE)
+  # } else {
+  #   # Sort by the value specified in the outcome param
+  #   outcome_sort <- sort(as.numeric(input_df[, column_name]))
+  #   # Make sure the row specified in the num param exists
+  #   if (length(outcome_sort) < num) {
+  #     return("NA")
+  #   }
+  #   # Select the outcome value of the specified rank
+  #   result <- outcome_sort[num]
+  # }
+  # 
+  # Select the row corresponding to the found value
+#   result_row <- subset(input_df, input_df[, column_name] == result)
+#   # Remove column label
+#   result_hospital <- unname(result_row[1, 'Hospital.Name'])
+#   
+#   
+#   print(result_hospital)
+#   result_hospital
+# }
