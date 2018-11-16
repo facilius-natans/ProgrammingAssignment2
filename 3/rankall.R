4bcf3579db866d380d0a823897b162c372457408
facilius-natans <facilius.natans@gmail.com>
2018-11-15 22:50:44 -0500

    Use lapply to handle "best" num value.

--- 3/rankall.R
+++ 3/rankall.R
@@ -18,8 +18,27 @@ rankall <- function(outcome, num = "best") {
   # Format NAs
   df[, column_name] <- gsub("Not Available", "NA", df[, column_name])
   
+  # Cast outcome as numeric
+  df[, column_name] <- suppressWarnings(as.numeric(df[, column_name]))
+  
+  # Sort data  by state, outcome, and hospital name
+  df <- df[order(df$"State", df[column_name], df$"Hospital.Name", na.last=NA),]
+  
   # Group data by state
   list_by_state <- split(df[,c("Hospital.Name")], df$State)
+  
+  # Function to rank hospitals in each state
+  rank_hospitals <- function(state_set, num){
+    if (num == "best") {
+      result <- head(state_set, 1)
+    }
+    else if (num == "worst") {
+      #
+    }
+    else {
+      #
+    }  
+  }
   
   # Condition based on num
   if (num == "best") {
@@ -32,7 +51,10 @@ rankall <- function(outcome, num = "best") {
     #
   }
   
-  return(length(list_by_state))
+  result <- lapply(list_by_state, rank_hospitals, num)
+  result <- data.frame(hospital = unlist(result), state = names(result), row.names = names(result))
+  
+  return(result)
   
   library(dplyr)
   if (outcome == "pneumonia") {
