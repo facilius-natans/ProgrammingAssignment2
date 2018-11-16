c0a729a5769b93b357c938c6deed0d7c173580b0
facilius-natans <facilius.natans@gmail.com>
2018-11-15 21:15:41 -0500

    Add error handling.

--- 3/rankall.R
+++ 3/rankall.R
@@ -4,8 +4,16 @@ rankall <- function(outcome, num){
   #read file
   df <- read.csv("outcome-of-care-measures.csv", colClasses = "character")
   
+  # Map argument to column_name
+  
   source("outcome_switch.R")
   column_name <- outcome_switch(outcome)
+  
+  # Error handling
+  
+  if (column_name == "NA") {
+    return(column_name)
+  }
   
   # Select the required columns: Hospital.Name, State, and the column name with desired outcome
   
