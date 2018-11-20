d84235662e8ecec2ad0953da17938f605e4276e9
facilius-natans <facilius.natans@gmail.com>
2018-11-20 19:52:08 +0000

    Working with excel files from Internet

---
+++ Getting_and_Cleaning_Data/Work_With_Excel_files.R
@@ -0,0 +1,23 @@
+
+  # use "xlsx" package
+  library("xlsx")
+
+  # Download excel file fro Internet
+  fileUrl <- "https://d396qusza40orc.cloudfront.net/getdata%2Fdata%2FDATA.gov_NGAP.xlsx"
+  download.file(fileUrl, destfile = "getdata%2Fdata%2FDATA.gov_NGAP.xlsx")
+              
+  # Record date and time
+  dateDownloaded <- date()
+  print(dateDownloaded)
+
+  # Read file
+  file <- read.xlsx("getdata%2Fdata%2FDATA.gov_NGAP.xlsx", sheetIndex = 1, header = TRUE)
+  #print(head(file))
+
+  # Extract 18-23 rows and 7-15 columns
+  dat <- file[18:23, 7:15]
+  # print(head(dat))
+
+  # get result from expression:
+  a <- sum(dat$Zip*dat$Ext, na.rm = T)
+  print(a)
