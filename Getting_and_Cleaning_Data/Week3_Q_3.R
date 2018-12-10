882db8a37fd6d48b20c28b5d54b2aa90538f4e37
facilius-natans <facilius.natans@gmail.com>
2018-12-10 20:33:30 +0000

    Working on week3-q3-5

--- Getting_and_Cleaning_Data/Week3_Q_3.R
+++ Getting_and_Cleaning_Data/Week3_Q_3.R
@@ -1,14 +1,15 @@
   # download file from internet
-  fileUrl <- "https://d396qusza40orc.cloudfront.net/getdata%2Fdata%2FGDP.csv"
+  #fileUrl <- "https://d396qusza40orc.cloudfront.net/getdata%2Fdata%2FGDP.csv"
  
   
    # Download into R
-   gdp <-download.file(fileUrl, destfile = "getdata%2Fdata%2FGDP.csv")
+   #gdp <-download.file(fileUrl, destfile = "getdata%2Fdata%2FGDP.csv")
    
    # read csv file
-   gdp <-  read.csv("getdata%2Fdata%2FGDP.csv", TRUE,  "," )
+   gdp <-  read.csv("getdata_data_GDP.csv", TRUE,  "," )
    
-   #print(head(gdp))
+   # print(head(gdp))
+   
    
    # download file from internet
    fileUrl1 <- "https://d396qusza40orc.cloudfront.net/getdata%2Fdata%2FEDSTATS_Country.csv"
@@ -21,14 +22,36 @@
     
    #print(head(edu))
     
-   combined_file <- (merge(x = gdp, y = edu, by.x = 1, by.y = 1))
-   #print(head(combined_file, 13))
-   # sort(as.numeric(input_df[, column_name]))
-   #  min(as.numeric(input_df[, column_name]), na.rm = TRUE)
-   #   df <- df[order(df$"State", df[column_name], df$"Hospital.Name", na.last=NA),]
-   cino <- combined_file[order(combined_file$"Gross.domestic.product.2012", na.last=NA),]
-   print(names(cino))
-   #print((combined_file$"Gross.domestic.product.2012"))
-   # match <- sum(match(gdp$x, edu$CountryCode))
-   #print(match)
+   combined_df <- (merge(x = gdp, y = edu, by.x = 1, by.y = 1))
+   #print(head(combined_df, 13))
+  
+   df <- combined_df[1:190, c("CountryCode", "Ranking", "millions.of.US.dollars", "Income.Group")]
+   #print(head(df))
    
+   df$CountryCode <-  as.character(df$CountryCode)
+   
+   df$Ranking <- as.numeric(df$Ranking)
+   
+   df$"millions.of.US.dollars" <- as.numeric(df$"millions.of.US.dollars")
+   
+   #str(df)
+   
+  
+   
+   df_sorted <- df[order(df$Ranking, decreasing = TRUE, na.last=TRUE), ]
+   #df_sorted <- df[order(df$Ranking), ]
+   #print(tail(df_sorted, 13))
+   
+   
+   #av_gtp <-tapply(df$Ranking, df$"Income.Group", ave)
+   gr <- group_by(df,df$"Income.Group")
+   av_gr <- summarise(gr, mean(Ranking, na.rm = TRUE ))
+   
+   #print(av_gr)
+   
+   quan_gr <- quantile(df$Ranking, na.rm = TRUE)
+   #print(quan_gr)
+   
+   table_with_quan <- within(df, as.integer(cut(df$Ranking, quantile(df$Ranking), na.rm = TRUE)))
+ 
+   print(table_with_quan)
